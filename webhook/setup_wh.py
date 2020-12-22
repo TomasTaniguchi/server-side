526b653016cc763cc137f8123d2f5fa97c568fec
denius <66587093+bueltan@users.noreply.github.com>
2020-12-22 15:17:48 -0300

    Add files via upload
    setup_wh add

---
+++ webhook/setup_wh.py
@@ -0,0 +1,39 @@
+from pyngrok import ngrok
+import requests
+import sys
+from api_maytapi import varibles
+
+# Replace the values here.
+INSTANCE_URL = varibles.INSTANCE_URL
+PRODUCT_ID = varibles.PRODUCT_ID
+API_TOKEN = varibles.API_TOKEN
+
+
+def setup_webhook():
+    if PRODUCT_ID == ""  or API_TOKEN == "":
+        print(
+            "You need to change PRODUCT_ID and API_TOKEN values in app.py file.", file=sys.stdout, flush=True
+        )
+        return
+
+    public_url = ngrok.connect(8000)
+    public_url = public_url.replace("http", "https", 1)
+    print("Public Url " + public_url, file=sys.stdout, flush=True)
+
+    url = INSTANCE_URL + "/" + PRODUCT_ID + "/setWebhook"
+
+
+    headers = {
+        "Content-Type": "application/json",
+        "x-maytapi-key": API_TOKEN,
+    }
+
+    body = {"webhook": public_url + "/webhook"}
+
+    response = requests.post(url, json=body, headers=headers)
+    print(response.json(), file=sys.stdout, flush=True)
+
+
+# Do not use this method in your production environment
+setup_webhook()
+
